d116c4cff15188fdbd9fc3e298176743045935e7
Thomas Lebeau <lebeau.thomas@gmail.com>
2013-11-01 16:58:15 +0200

    Add git hook deployment script

--- Gruntfile.js
+++ Gruntfile.js
@@ -238,7 +238,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,php}',
             '.htaccess',
             'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
