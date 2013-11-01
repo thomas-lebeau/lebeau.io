81b3f51db0d0a902d198e3d3becceb60e757d2ed
Thomas Lebeau <lebeau.thomas@gmail.com>
2013-11-01 12:29:14 +0200

    copy task now copy the full twitter folder including subfloders. Fix #6

--- Gruntfile.js
+++ Gruntfile.js
@@ -243,7 +243,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'styles/fonts/*',
-            'twitter/*'
+            'twitter/**/*'
           ]
         }, {
           expand: true,
