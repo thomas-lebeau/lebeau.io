37528be2cfa237929b901b5005b3472511b5374a
Thomas Lebeau <lebeau.thomas@gmail.com>
2013-11-19 15:47:55 +0200

    Add Sevim's testimonial

--- app/scripts/services/testimonials.js
+++ app/scripts/services/testimonials.js
@@ -4,15 +4,11 @@ angular.module('portfolioApp')
   .service('Testimonials', function Testimonials() {
   var testimonials = [
       {
-        name: 'John Doe',
-        company: 'Company Name',
-        companyLink: 'http://google.com',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ipsa natus temporibus culpa provident mollitia assumenda possimus quidem cumque deserunt distinctio aspernatur impedit illum consectetur adipisci animi quisquam accusantium consequatur?'
-      }, {
-        name: 'Steve Jobs',
-        company: 'Elmas Inc.',
-        companyLink: 'http://google.com',
-        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ipsa natus temporibus culpa provident.'
+        name: 'Sevim Tokmak',
+        company: 'Aleni Reklam',
+        companyLink: 'http://alenireklam.com',
+        textTR: 'İhtiyaçlarımız konusunda,  bize olan desteğinden dolayı teşekkür ediyoruz. Kalite, uyum, birlikte çalışabilme konularında ki özen ve başarısı ile iyi bir çözüm ortağı.',
+        text: 'We thank Thomas for his support in our job. His quality work and professionalism makes him a good business partner'
       }
     ];
 
