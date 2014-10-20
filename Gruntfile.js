100f3485532a329b444a6899f2146a9125950930
Thomas Lebeau <lebeau.thomas@gmail.com>
2014-10-20 16:59:12 +0300

    fixes: CNAME file is not copied into the dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -244,6 +244,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt,php,xml}',
             '.htaccess',
+            'CNAME'
             // 'bower_components/**/*',
             'images/{,*/}*.{gif,webp}',
             'fonts/**/*',
